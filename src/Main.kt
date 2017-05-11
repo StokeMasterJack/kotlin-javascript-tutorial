763b52c45d6a40930ef9928e62dc159713fe6ba3
David Ford <dford@smart-soft.com>
2017-05-11 16:01:39 -0700

    End of Lesson 3: Working with the DOM - Part 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,20 @@
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    println("Hello")
-
-    val x = 10
-
-    val html =
-"""
-<h1>Hello</h1>
-<p>$x</p>
-<button id="b1">Click Me</button>
-"""
 
     val root = document.getElementById("root")
-    root!!.innerHTML = html
-    val b1 = document.getElementById("b1")
-    b1!!.addEventListener("click",{ println("clicked") })
+
+    val div = document.createElement("div")
+    val h1 = document.createElement("h1")
+    val b1 = document.createElement("button")
+
+    h1.textContent = "Hello 2"
+    b1.textContent = "Click Me 2"
+    b1.addEventListener("click",{ println("clicked 2")})
+
+    div.appendChild(h1)
+    div.appendChild(b1)
+
+    root!!.appendChild(div)
 
 }
