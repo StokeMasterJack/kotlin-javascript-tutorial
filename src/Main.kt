a49ba4f8fe628498c605340b7029f9711e2b729d
David Ford <dford@smart-soft.com>
2017-05-11 09:26:41 -0700

    End of Lesson 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
+import kotlin.browser.document
+
 fun main(args: Array<String>) {
     println("Hello")
+
+    val x = 10
+
+    val html =
+"""
+<h1>Hello</h1>
+<p>$x</p>
+<button id="b1">Click Me</button>
+"""
+
+    val root = document.getElementById("root")
+    root!!.innerHTML = html
+    val b1 = document.getElementById("b1")
+    b1!!.addEventListener("click",{ println("clicked") })
+
 }
