e9e4442d41b6ea2a84b5d3d119d000e936a890c0
David Ford <dford@smart-soft.com>
2017-05-11 17:58:03 -0700

    End of Lesson 4: Working with the DOM - Part 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,24 @@
+import kotlinx.html.button
+import kotlinx.html.div
+import kotlinx.html.dom.create
+import kotlinx.html.h1
+import kotlinx.html.js.onClickFunction
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
 
     val root = document.getElementById("root")
 
-    val div = document.createElement("div")
-    val h1 = document.createElement("h1")
-    val b1 = document.createElement("button")
 
-    h1.textContent = "Hello 2"
-    b1.textContent = "Click Me 2"
-    b1.addEventListener("click",{ println("clicked 2")})
-
-    div.appendChild(h1)
-    div.appendChild(b1)
+    val div = document.create.div{
+        h1{
+            +"Hello Kotlinx Html"
+        }
+        button{
+            +"Click Me"
+            onClickFunction = { println("Clicked Again")}
+        }
+    }
 
     root!!.appendChild(div)
 
